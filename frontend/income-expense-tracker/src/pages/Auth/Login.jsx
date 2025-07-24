26f07341a163ddb9ed1e8d816e70149c3df12940
connorhills <connorhills2023@gmail.com>
2025-07-23 22:19:00 -0400

    Fixed Login form UI on mobile

--- frontend/income-expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/income-expense-tracker/src/pages/Auth/Login.jsx
@@ -69,41 +69,54 @@ const Login = () => {
 
   return (
     <AuthLayout>
-      <div className="lg:w-[100%] h-[50%] mt-[50%] lg:mt-[20%] flex flex-col justify-center border-2 shadow-lg shadow-primary border-primary rounded-4xl px-20">
-        <h3 className="text-xl font-semibold text-white">Welcome Back!</h3>
-        <p className="text-xs text-blue-300 mt-[5px] mb-6">
-          Please enter your details to login
-        </p>
-
-        <form onSubmit={handleLogin}>
-          <Input
-            value={email}
-            onChange={({ target }) => setEmail(target.value)}
-            label="Email Address"
-            placeholder="johnsmith@gmail.com"
-            type="text"
-          />
-          <Input
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-            label="Password"
-            placeholder="Min 8 characters"
-            type="password"
-          />
-
-          {error && <p className="text-red-600 text-xs pb-2.5">{error}</p>}
-
-          <button type="submit" className="btn-primary cursor-pointer">
-            LOGIN
-          </button>
-
-          <p className="text-[13px] text-slate-200 mt-3">
-            Don't have an account?{" "}
-            <Link className="font-medium text-primary underline" to="/signup">
-              Sign Up
-            </Link>
+      <div className="min-h-screen flex items-center justify-center -mt-10 sm:-mt-16">
+        <div
+          className="
+        w-full max-w-md
+        flex flex-col justify-center
+        border-2 shadow-lg shadow-primary border-primary rounded-4xl
+        px-4 py-8 sm:px-8
+        bg-black bg-opacity-80
+      "
+        >
+          <h3 className="text-xl font-semibold text-white">Welcome Back!</h3>
+          <p className="text-xs text-blue-300 mt-1 mb-6">
+            Please enter your details to login
           </p>
-        </form>
+
+          <form onSubmit={handleLogin}>
+            <Input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="johnsmith@gmail.com"
+              type="text"
+            />
+            <Input
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              label="Password"
+              placeholder="Min 8 characters"
+              type="password"
+            />
+
+            {error && <p className="text-red-600 text-xs pb-2.5">{error}</p>}
+
+            <button
+              type="submit"
+              className="btn-primary cursor-pointer w-full mt-2"
+            >
+              LOGIN
+            </button>
+
+            <p className="text-[13px] text-slate-200 mt-3 text-center">
+              Don't have an account?{" "}
+              <Link className="font-medium text-primary underline" to="/signup">
+                Sign Up
+              </Link>
+            </p>
+          </form>
+        </div>
       </div>
     </AuthLayout>
   );
